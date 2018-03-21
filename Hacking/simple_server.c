17214bcf6b420c8738ba1d398e80951e1ed238eb
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-03-21 11:51:47 -0600

    Update simple_server.c

--- Hacking/simple_server.c
+++ Hacking/simple_server.c
@@ -11,7 +11,7 @@
 
 int main(void) {
 	int sockfd, new_sockfd;  // listen on sock_fd, new connection on new_fd
-	struct sockaddr_in host_addr, client_addr;	// my address information
+	struct sockaddr_in host_addr, client_addr;
 	socklen_t sin_size;
 	int recv_length=1, yes=1;
 	char buffer[1024];
@@ -24,8 +24,8 @@ int main(void) {
 
 	host_addr.sin_family = AF_INET;		 // host byte order
 	host_addr.sin_port = htons(PORT);	 // short, network byte order
-	host_addr.sin_addr.s_addr = INADDR_ANY; // automatically fill with my IP
-	memset(&(host_addr.sin_zero), '\0', 8); // zero the rest of the struct
+	host_addr.sin_addr.s_addr = INADDR_ANY;  // automatically fill with my IP
+	memset(&(host_addr.sin_zero), '\0', 8);  // zero the rest of the struct
 
 	if (bind(sockfd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr)) == -1)
 		fatal("binding to socket");
@@ -33,14 +33,18 @@ int main(void) {
 	if (listen(sockfd, 5) == -1)
 		fatal("listening on socket");
 
-	while(1) {    // Accept loop
+	while(1) {
 		sin_size = sizeof(struct sockaddr_in);
 		new_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
+		
 		if(new_sockfd == -1)
 			fatal("accepting connection");
+	
 		printf("server: got connection from %s port %d\n",inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+		
 		send(new_sockfd, "Hello World!\n", 13, 0);
 		recv_length = recv(new_sockfd, &buffer, 1024, 0);
+		
 		while(recv_length > 0) {
 			printf("RECV: %d bytes\n", recv_length);
 			dump(buffer, recv_length);
