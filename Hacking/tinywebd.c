0c1e1c481c90be3641c29669e76d6cf2971145dc
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-05-23 09:46:35 -0600

    Update tinywebd.c

--- Hacking/tinywebd.c
+++ Hacking/tinywebd.c
@@ -35,19 +35,19 @@ int main(void) {
     socklen_t sin_size;
 
     logfd = open(LOGFILE, O_WRONLY|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);
-    if(logfd == -1)
-      fatal("opening log file");
-
-    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1)
-      fatal("in socket");
-
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
-      fatal("setting socket option SO_REUSEADDR");
-
+    if(logfd == -1) {
+        fatal("opening log file");
+    }
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+        fatal("in socket");
+    }
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+        fatal("setting socket option SO_REUSEADDR");
+    }
     printf("Starting tiny web daemon.\n");
-    if (daemon(1, 0) == -1)
+    if (daemon(1, 0) == -1) {
         fatal("forking to daemon process");
-
+    }
     signal(SIGTERM, handle_shutdown);
     signal(SIGINT, handle_shutdown);
 
@@ -59,18 +59,20 @@ int main(void) {
     host_addr.sin_addr.s_addr = INADDR_ANY; // Automatically fill with my IP
     memset(&(host_addr.sin_zero), '\0', 8); // Zero the rest of the struct
 
-    if (bind(sockfd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr)) == -1)
+    if (bind(sockfd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr)) == -1) {
         fatal("binding to socket");
+    }
 
-    if (listen(sockfd, 20) == -1)
+    if (listen(sockfd, 20) == -1) {
         fatal("listening on socket");
+    }
 
     while(1) {
         sin_size = sizeof(struct sockaddr_in);
         new_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
-        if(new_sockfd == -1)
+        if(new_sockfd == -1) {
             fatal("accepting connection");
-
+        }
         handle_connection(new_sockfd, &client_addr, logfd);
     }
     return 0;
@@ -95,15 +97,18 @@ ntohs(client_addr_ptr->sin_port), request);
     } else {
         *ptr = 0;
         ptr = NULL;
-        if (strncmp(request "GET ", 4) == 0)
+        if (strncmp(request "GET ", 4) == 0) {
             ptr = request + 4;
-        if (strncmp(request "HEAD ", 5) == 0)
+        }
+        if (strncmp(request "HEAD ", 5) == 0) {
             ptr = request + 5;
+        }
         if (ptr == NULL) {
             strcat(log_buffer, " UNKNOWN REQUEST!\n");
         } else {
-            if (ptr[strlen(ptr) - 1] == '/')
+            if (ptr[strlen(ptr) - 1] == '/') {
                 strcat(ptr, "index.html");
+            }
             strcpy(resource, WEBROOT);
             strcat(resource, ptr);
             fd = open(resource, O_RDONLY, 0)
@@ -119,10 +124,12 @@ ntohs(client_addr_ptr->sin_port), request);
                 send_string(sockfd, "HTTP/1.0 200 OK\r\n");
                 send_string(sockfd, "Server: Tiny webserver\r\n\r\n");
                 if (ptr == request + 4) {
-                    if ((length = get_file_size(fd)) == -1)
+                    if ((length = get_file_size(fd)) == -1) {
                         fatal("getting resource file size");
-                    if ((ptr = (unsigned char *) malloc(length)) == NULL)
+                    }
+                    if ((ptr = (unsigned char *) malloc(length)) == NULL) {
                         fatal("allocating memory for reading resource");
+                    }
                     read(fd, ptr, length);
                     send(sockfd, ptr, length, 0);
                     free(ptr);
@@ -143,9 +150,9 @@ ntohs(client_addr_ptr->sin_port), request);
  */
 int get_file_size(int fd) {
     struct stat stat_struct;
-
-    if (fstat(fd, &stat_struct) == -1)
+    if (fstat(fd, &stat_struct) == -1) {
         return -1;
+    }
     return (int) stat_struct.st_size;
 }
 
