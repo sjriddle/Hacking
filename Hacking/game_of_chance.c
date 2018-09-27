c4fd38a041357215a7b41c3baadf9ce3bc9ff463
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-09-27 07:55:31 -0600

    Update game_of_chance.c

--- Hacking/game_of_chance.c
+++ Hacking/game_of_chance.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include "hacking.h"
 
-#define DATAFILE "/var/chance.data" // File to store user data
+#define DATAFILE "/var/chance.data"
 
 // Custom user struct to store information about users
 struct user {
@@ -33,7 +33,7 @@ int find_the_ace();
 void fatal(char *);
 
 // Global variables
-struct user player;      // Player struct
+struct user player;
 
 int main() {
    int choice, last_game;
