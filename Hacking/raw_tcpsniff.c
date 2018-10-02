8182df27cb125fbae5cb600a39e59eeb4910056e
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-10-02 07:09:07 -0600

    Update raw_tcpsniff.c

--- Hacking/raw_tcpsniff.c
+++ Hacking/raw_tcpsniff.c
@@ -4,13 +4,11 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
 #include "hacking.h"
 
 int main(void) {
     int i, recv_length, sockfd;
     u_char buffer[9000];
-
     if ((sockfd = socket(PF_INIT, SOCK_RAW, IPPROTO_TCP)) == -1) {
         fatal("in socket");
     }
