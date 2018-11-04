6bcbd698f369b73516588aec0f8fabf67ac054e6
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-11-04 09:53:35 -0700

    Update hacking-network.h

--- Hacking/hacking-network.h
+++ Hacking/hacking-network.h
@@ -1,6 +1,7 @@
 /* This function accepts a socket FD and a ptr to the null terminated
- * string to send.  The function will make sure all the bytes of the
- * string are sent.  Returns 1 on success and 0 on failure.
+ * string to send.  
+ * The function will make sure all the bytes of the string are sent.
+ * Returns 1 on success and 0 on failure.
  */
 int send_string(int sockfd, unsigned char *buffer) {
    int sent_bytes, bytes_to_send;
@@ -16,7 +17,7 @@ int send_string(int sockfd, unsigned char *buffer) {
 }
 
 /* This function accepts a socket FD and a ptr to a destination
- * buffer.  It will receive from the socket until the EOL byte
+ * buffer. It will receive from the socket until the EOL byte
  * sequence in seen.  The EOL bytes are read from the socket, but
  * the destination buffer is terminated before these bytes.
  * Returns the size of the read line (without EOL bytes).
@@ -38,13 +39,13 @@ int recv_line(int sockfd, unsigned char *dest_buffer) {
       } else {
          eol_matched = 0;
       }
-      ptr++; // increment the pointer to the next byter;
+      ptr++; 
    }
-   return 0; // didn't find the end of line characters
+   return 0; 
 }
 
 
-/* Structure for Ethernet headers */
+// Structure for Ethernet headers
 #define ETHER_ADDR_LEN 6
 #define ETHER_HDR_LEN 14
 
