912b57bd1b1d2b51efa1a197728e748a22f93638
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2019-03-18 14:40:47 -0600

    Update host_lookup.c

--- Hacking/host_lookup.c
+++ Hacking/host_lookup.c
@@ -10,10 +10,12 @@
 int main(int argc, char *argv[]) {
     struct hostent *host_info;
     struct in_addr *address;
+    
     if(argc < 2) {
         printf("Usage: %s <hostname>\n", argv[0]);
         exit(1);
     }
+    
     host_info = gethostbyname(argv[1]);
     if(host_info == NULL) {
         printf("Couldn't lookup %s\n", argv[1]);
